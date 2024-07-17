ee1a17812ec8758b1192de5365736568f047e5a0
Moisés Medeiros <52640097+Moises788@users.noreply.github.com>
2024-07-17 20:21:54 -0300

    Add files via upload

---
+++ Files/main.py
@@ -0,0 +1,14 @@
+#criar uma classe para importação dos dados (construtor da classe)
+#Após isso, método para pré-processamento adequado  dos daods (talvez um método para cada modelo)
+#Métodos que retornam os gráficos para interface dos dados
+
+#Classe para treinamento e validaçãp dos modelos desenvolvidos
+#Método que retorna validação dos modelos
+#Hierarquia dos dados pré-processados
+#treinamento e validação via métodos
+
+#Classe mãe que realiza a interface gráfica
+#importa o modelo já treinado
+#Mostra gráficos dos dados antigos
+#importa dados novos para modelo
+#retorna resultado do modelo já treinado
